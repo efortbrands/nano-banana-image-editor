abbffd96ca48a3aa54a777158735017d1327d743
E-Fort Brands <e-fortbrands@E-Forts-Mac-mini.local>
2025-12-24 08:39:47 +0300

    Remove standalone output for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Required for Docker deployment
   images: {
     remotePatterns: [
       {
